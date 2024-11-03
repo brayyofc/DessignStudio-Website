0d7af0bdff2a630fe85e5f4aca32ff8009292713
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-11-03 20:51:30 +0530

    Contact Us Responsive- Tab and Mobile view

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -13,24 +13,26 @@ function Contact() {
         <p>›</p>
         <h1 className="font-sans">Contact Us</h1>
       </div>
-      <div className="flex px-20 py-10">
-        <div className="w-1/2">
-          <h1 className="font-bold text-4xl py-5">Keep in touch with us</h1>
-          <p className="text-base py-5">
+      <div className="flex flex-col lg:flex-row justify-center px-10 md:px-20 py-5 md:py-10 gap-10">
+        <div className="lg:w-1/2">
+          <h1 className="font-bold text-3xl md:text-4xl py-2 md:py-5">
+            Keep in touch with us
+          </h1>
+          <p className="text-base py-2 md:py-5">
             If your requirements are outside or capabilities, we believe in
             turning away business with integrity. But if your requirements are
             within our capabilities, we're probably one of the few organizations
             that can guarantee satisfaction and results
           </p>
-          <div className="flex w-full py-5">
-            <div className="w-1/2 ">
-              <h3 className="font-bold py-5">Address</h3>
-              <p className="text-base w-1/2">
+          <div className="flex flex-col md:flex-row w-full py-5">
+            <div className="md:w-1/2">
+              <h3 className="font-bold py-3 md:py-5">Address</h3>
+              <p className="text-base md:w-1/2">
                 155 Indra Park, New Delhi 110059
               </p>
             </div>
-            <div className="w-1/2">
-              <h3 className="font-bold py-5">Contact</h3>
+            <div className="md:w-1/2">
+              <h3 className="font-bold py-3 md:py-5">Contact</h3>
               <p className="text-base">
                 Mobile: <strong>+91-9910759139</strong>
               </p>
@@ -40,9 +42,11 @@ function Contact() {
             </div>
           </div>
         </div>
-        <div className="w-1/2">
-          <h1 className="font-bold text-4xl py-5">Send a message</h1>
-          <div className="flex w-full py-5 justify-between gap-8">
+        <div className="lg:w-1/2">
+          <h1 className="font-bold text-3xl md:text-4xl py-2 md:py-5">
+            Send a message
+          </h1>
+          <div className="flex w-full py-5 justify-between gap-4 md:gap-8">
             <input
               type="text"
               placeholder="Name"
@@ -73,7 +77,7 @@ function Contact() {
           </div>
         </div>
       </div>
-      <div className="mt-20">
+      <div className="mt-5 md:mt-20">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d309.5271101138193!2d77.30954042885176!3d28.632774140109593!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce4b2f79b09d5%3A0x158880a7f1a9f5c4!2sI.P.Extension%2C%20Patparganj%2C%20Delhi!5e0!3m2!1sen!2sin!4v1730644497140!5m2!1sen!2sin"
           height="600"
